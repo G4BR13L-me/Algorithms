52931f7c9a3e12699b32ea142bf34de01604d884
Gabriel Moreira <gmmoreira11@gmail.com>
2023-05-25 15:34:11 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define PB push_back
+#define ALL(x) x.begin(), x.end()
+#define GET_MACRO(_1,_2,_3,NAME,...) NAME
+#define FOR(...) GET_MACRO(__VA_ARGS__, FOR2, FOR1, _UNUSED)(__VA_ARGS__)
+#define FOR1(i, n) for (int i = 0; i < (int)n; i++)
+#define FOR2(i, a, b) for (int i = (int)a; i < (int)b; i++)
+#define ROF(...) GET_MACRO(__VA_ARGS__, ROF2, ROF1, _UNUSED)(__VA_ARGS__)
+#define ROF1(i, n) for (int i = (int)n-1; i >= 0; i--)
+#define ROF2(i, a, b) for (int i = (int)b; i >= (int)a; i--)
+#define SORT(v) sort(ALL(v))
+#define SORTD(v) sort(ALL(v), greater<int>())
+#define mem0(a) memset(a,0,sizeof(a))
+#define mem_1(a) memset(a,-1,sizeof(a))
+#define meminf(a) memset(a,0x3f,sizeof(a))
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3fLL
+#define MAXN 110
+#define EPS 1e-6
+#define PI 3.14159265359
+
+int main()
+{	
+
+	return 0;
+}
